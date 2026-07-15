b9b8f9291a8c5aec02a5d2436e1ad8df8fcdf1e4
ayurved-vigyan <ayurvedsevigyantak@gmail.com>
2026-07-16 00:36:47 +0530

    Update features-config.js

--- features-config.js
+++ features-config.js
@@ -1,3 +1,11 @@
 // Ayurved se Vigyan Tak - Master Config & Registry
-const MasterConfig = {};
-const FeatureRegistry = {};
+const MasterConfig = {
+    blogName: "Ayurved se Vigyan Tak",
+    themeColor: "dark-green",
+    isMaintenanceMode: false
+};
+
+const FeatureRegistry = {
+    enableQuickShare: true,
+    enableReadingTime: true
+};
